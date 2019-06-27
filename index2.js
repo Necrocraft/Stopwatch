8abebf87526a90dbdf58234ad4f8eff9d7423f8c
Necrocraft <mhdzeefan@gmail.com>
2019-06-27 13:09:36 +0530

    ok

--- index2.js
+++ index2.js
@@ -2,3 +2,8 @@ function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName)
 }
 greet('John', 'Smith');
+
+function square(number) {
+    return number*number;
+}
+console.log(square(2));
