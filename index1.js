7c9ab50fb7a004f8d7ffd525b995511d86360728
Necrocraft <mhdzeefan@gmail.com>
2019-06-27 12:35:15 +0530

    Arrays

---
+++ index1.js
@@ -0,0 +1,5 @@
+let selectedColors = ['red', 'green']; //Array is dynamic and cn store multiple type values as given
+selectedColors[2] = '10';
+console.log(selectedColors);
+console.log(selectedColors.length);
+console.log(selectedColors[1]);
