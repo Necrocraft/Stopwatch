a0e3fa0ab22a41c177212787dc28c7e50114ae82
Necrocraft <mhdzeefan@gmail.com>
2019-06-27 12:25:02 +0530

    Object

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let person = {
     age: 25
 };
 // Dot Notation
-person.name = 'John';
+person.name = 'John'; // For this we can use console.log(person.name);
+// For running the complete object just use console.log(person);
 // Bracket Notation
 person['name'] = 'Mary';
-
 console.log(person['name']);
