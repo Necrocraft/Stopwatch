b4804edeff95d167c7d1c17eaaa836348873ebc5
Mohd Zeefan <mhdzeefan@gmail.com>
2019-06-29 20:04:52 +0530

    Delete index1.js

--- index1.js
+++
@@ -1,5 +0,0 @@
-let selectedColors = ['red', 'green']; //Array is dynamic and cn store multiple type values as given
-selectedColors[2] = '10';
-console.log(selectedColors);
-console.log(selectedColors.length);
-console.log(selectedColors[1]);
