7ce3aada652d0521646c06d9d3dca9de9d1ea2cc
Mohd Zeefan <mhdzeefan@gmail.com>
2019-06-29 20:05:01 +0530

    Delete index2.js

--- index2.js
+++
@@ -1,9 +0,0 @@
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName)
-}
-greet('John', 'Smith');
-
-function square(number) {
-    return number*number;
-}
-console.log(square(2));
