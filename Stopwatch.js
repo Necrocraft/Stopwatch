29599c3b2adfa419327700980f26aec62bcb2aca
Necrocraft <mhdzeefan@gmail.com>
2019-06-29 14:36:54 +0530

    yo

---
+++ Stopwatch.js
@@ -0,0 +1,44 @@
+
+
+function stopWatch() {
+
+    let startTime, endTime, running;
+    let duration = 0;
+
+    return {
+    start: function() {
+        if (running)
+            throw new Error('Stopwatch is already running');
+        
+        running = true;
+        
+        startTime = new Date();
+
+    },
+
+    stop: function() {
+        if (!running)
+            throw new Error('Stopwatch has already stopped');
+        
+        running = false;
+        
+        endTime = new Date();
+
+        const sec = (endTime.getTime() - startTime.getTime())/1000;
+        
+        duration += sec;
+    },
+
+    getDuration: function() {
+        return duration;
+    },
+
+    reset: function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    },
+};
+}
+const sw = stopWatch();
